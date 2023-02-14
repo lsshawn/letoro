b053d4ec95d8f943bcc5326c18dadccdc0c0592c
sshawn <coffee.or.tea88@gmail.com>
2023-02-14 12:03:31 +0800

    whatsapp to each branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,24 +12,27 @@
 
 	const locations = [
 		{
-			title: 'HQ',
+			title: 'HQ (Jalan Puchong)',
 			address: 'LOT 4211A, TAMAN PAIK SIONG, 7 1/2 MILES, JALAN PUCHONG, 47100 PUCHONG SELANGOR',
 			phone: '603 8080 0507',
+			whatsapp: '60162770507',
 			map: 'https://www.google.com/maps/dir//LeToro+Grooming,+Lot+4211A,+Taman+Paik+Siong,+7+1%2F2+miles,+Jalan+Puchong,+47100+Puchong,+Selangor/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x31cc4b11763505f1:0xa59760f03c65418f?sa=X&ved=2ahUKEwj-woL7_a_oAhW1IbcAHcuiBO0Q48ADMAB6BAgIECk',
 			hours: '10.00AM – 7.00PM'
 		},
 		{
-			title: 'BP Outlet',
+			title: 'Bandar Puteri Puchong',
 			address: 'NO 72G, JALAN PUTERI 5/5, BANDAR PUTERI PUCHONG, 47100 PUCHONG SELANGOR',
 			map: 'https://www.google.com/maps/dir//Letoro+Grooming+Bandar+Puteri+Puchong,+72G,+Jalan+Puteri+5%2F5,+Bandar+Puteri+Puchong,+47100+Puchong,+Selangor/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x31cdb4a06ce4c4e3:0x17a3ab41774e7cd9?sa=X&ved=2ahUKEwj-woL7_a_oAhW1IbcAHcuiBO0Q48ADMAF6BAgIEDQ',
 			phone: '603 8066 2311',
+			whatsapp: '60182202311',
 			hours: '9.00AM – 6.00PM'
 		},
 		{
-			title: 'HG Outlet',
+			title: 'Happy Garden',
 			map: 'https://www.google.com/maps/dir//Letoro+Grooming+Bandar+Puteri+Puchong,+72G,+Jalan+Puteri+5%2F5,+Bandar+Puteri+Puchong,+47100+Puchong,+Selangor/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x31cdb4a06ce4c4e3:0x17a3ab41774e7cd9?sa=X&ved=2ahUKEwj-woL7_a_oAhW1IbcAHcuiBO0Q48ADMAF6BAgIEDQ',
 			address: 'NO 39, JALAN LAZAT 1, TAMAN GEMBIRA, 58200 PUCHONG SELANGOR',
 			phone: '603 2385 6069',
+			whatsapp: '60186686069',
 			hours: '10.00AM – 7.00PM'
 		}
 	];
@@ -41,16 +44,25 @@
 		style="background-image: url(hero-dog.jpg);"
 	>
 		<div class="hero-overlay bg-opacity-80" />
-		<div class="hero-content items-start text-left text-neutral-content pr-0 min-w-full pt-[10rem]">
+		<div class="hero-content items-start text-left text-neutral-content pr-0 min-w-full pt-[5rem]">
 			<div class="w-full pb-4" style="text-shadow: 0 0 20px #000;">
-				<h1 class="mb-5 text-7xl ">Premium Pet Services</h1>
+				<h1 class="mb-5 text-6xl ">Premium Pet Services</h1>
 				<p class="mb-12 text-2xl max-w-md" style="text-shadow: 2px 2px 5px #121212">
 					We provide professional grooming service for your pets in a calm environment.
 				</p>
-				<a href="https://api.whatsapp.com/send?phone=60162770507&text=" target="_blank">
-					<button class="btn btn-outline btn-base">Book via Whatsapp</button>
-					<div class="font-thin mt-2">+6016 277 0507</div>
-				</a>
+				<div class="dropdown">
+					<label tabindex="0" class="btn btn-outline btn-base">Book via Whatsapp</label>
+					<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+						{#each locations as location}
+							<li>
+								<a
+									href={`https://api.whatsapp.com/send?phone=${location.whatsapp}&text=`}
+									target="_blank">{location.title}</a
+								>
+							</li>
+						{/each}
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -148,7 +160,7 @@
 
 <section id="shop">
 	<div
-		class="hero min-h-[80vh] place-items-start bg-contain bg-no-repeat bg-[#161616] bg-bottom"
+		class="hero min-h-[70vh] place-items-start bg-contain bg-no-repeat bg-[#161616] bg-bottom"
 		style="background-image: url(dog-high-contrast-no-pattern.jpg);"
 	>
 		<!-- <div class="hero-overlay bg-opacity-50" /> -->
@@ -182,7 +194,12 @@
 							<div>
 								{location.address}
 							</div>
-							<a href="tel:">
+							<a
+								href={`https://api.whatsapp.com/send?phone=${location.whatsapp}&text=`}
+								target="_blank">Book Now</a
+							>
+							<span class="mx-2">|</span>
+							<a href={`${location.tel}`}>
 								{location.phone}
 							</a>
 							<span class="mx-2">|</span>
