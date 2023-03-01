3aaf18e96dcfa6dd69c1c0876166c2469e1914e0
sshawn <coffee.or.tea88@gmail.com>
2023-03-01 18:37:46 +0800

    meta

---
+++ src/lib/components/MetaTag.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { MetaTags } from 'svelte-meta-tags';
+
+	const url = 'https://rvius.com';
+
+	export let customMeta = {};
+	const meta = {
+		title: 'Letoro Grooming',
+		desc: 'Boarding, Boarding, Spa, Scaling',
+		url: $page.url.href,
+		image: url + '/shop.jpg',
+		...customMeta
+	};
+</script>
+
+<MetaTags
+	title={meta.title}
+	titleTemplate={`${meta.title} | ${meta.desc}`}
+	description={meta.desc}
+	canonical={meta.url}
+	openGraph={{
+		url: meta.url,
+		title: meta.title,
+		description: meta.desc,
+		type: 'website',
+		images: [
+			{
+				url: meta.image,
+				width: 800,
+				height: 600,
+				alt: meta.title
+			}
+		],
+		site_name: meta.title
+	}}
+/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,10 @@
 <script>
 	import '../app.postcss';
 	import Logo from '$lib/assets/icons/logo.svg?component';
+	import MetaTag from '$lib/components/MetaTag.svelte';
 </script>
 
-<!-- <div class="navbar"> -->
-<!-- 	<div class="navbar-start" /> -->
-<!-- 	<div class="flex flex-col"> -->
-<!-- 		<a class="btn btn-ghost normal-case text-xl" href="/"> -->
-<!-- 			<Logo class="fill-gray-800" width="200" /> -->
-<!-- 		</a> -->
-<!-- 		<div class="flex gap-4 justify-space-around mt-4"> -->
-<!-- 			<h2 class="text-xl my-3 font-light">About Us</h2> -->
-<!-- 			<h2 class="text-xl my-3 font-light">Services</h2> -->
-<!-- 			<h2 class="text-xl my-3 font-light">Book Now</h2> -->
-<!-- 		</div> -->
-<!-- 	</div> -->
-<!-- 	<div class="navbar-end" /> -->
-<!-- </div> -->
+<MetaTag />
 <div class="navbar" style="background-color:#161516">
 	<div class="navbar-start" />
 	<Logo class="w-[200px] md:w-[500px] fill-white" />
